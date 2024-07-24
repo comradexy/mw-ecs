22a799de036192e88a753157f0802ecf4345b685
dxy <daixianyou@zjut.edu.cn>
2024-07-24 19:57:15 +0800

    feat: 添加定时任务注解

---
+++ scheduled-task-manager-starter/src/main/java/cn/comradexy/middleware/sdk/annatation/EnableSchedulingWithMgr.java
@@ -0,0 +1,12 @@
+package cn.comradexy.middleware.sdk.annatation;
+
+/**
+ * 开启使用管理中心托管的定时任务
+ *
+ * @Author: ComradeXY
+ * @CreateTime: 2024-07-24
+ * @Description: 开启使用管理中心托管的定时任务
+ */
+public @interface EnableSchedulingWithMgr {
+
+}
---
+++ scheduled-task-manager-starter/src/main/java/cn/comradexy/middleware/sdk/annatation/ScheduledWithMgr.java
@@ -0,0 +1,12 @@
+package cn.comradexy.middleware.sdk.annatation;
+
+/**
+ * 定时方法--使用管理中心托管
+ *
+ * @Author: ComradeXY
+ * @CreateTime: 2024-07-24
+ * @Description: 定时方法--使用管理中心托管
+ */
+public @interface ScheduledWithMgr {
+
+}
