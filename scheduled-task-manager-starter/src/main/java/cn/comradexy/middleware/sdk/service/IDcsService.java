5e241a9fe2db7f28e3f084c2e16efaf568af4e0b
dxy <daixianyou@zjut.edu.cn>
2024-08-11 21:13:43 +0800

    feat: 增加服务接口

--- scheduled-task-manager-starter/src/main/java/cn/comradexy/middleware/sdk/service/IDcsService.java
+++ scheduled-task-manager-starter/src/main/java/cn/comradexy/middleware/sdk/service/IDcsService.java
@@ -1,11 +1,11 @@
 package cn.comradexy.middleware.sdk.service;
 
 /**
- * 分布式服务
+ * 分布式服务接口
  *
  * @Author: ComradeXY
  * @CreateTime: 2024-08-08
- * @Description: 分布式服务
+ * @Description: 分布式服务接口
  */
 public interface IDcsService {
 
