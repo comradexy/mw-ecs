b554e3aa326efb1be835e6499bad37058921d337
dxy <daixianyou@zjut.edu.cn>
2024-08-15 08:34:08 +0800

    feat: 添加mapper

---
+++ scheduled-task-manager-starter/src/main/java/cn/comradexy/middleware/sdk/support/storage/jdbc/mapper/JobMapper.java
@@ -0,0 +1,14 @@
+package cn.comradexy.middleware.sdk.support.storage.jdbc.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Job对象关系映射
+ *
+ * @Author: ComradeXY
+ * @CreateTime: 2024-08-15
+ * @Description: Job对象关系映射
+ */
+@Mapper
+public interface JobMapper {
+}
